8d094268eedcc236e779ab0bdf497afd819d2f66
Yashvi1730 <171220055@nitdelhi.ac.in>
2020-06-18 10:37:55 -0700

    Updates

---
+++ precache-manifest.995e6058a8c27d6a72b9d456ead1ae2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e6c880613e8a010c2def386cc41878",
+    "url": "/feedback/index.html"
+  },
+  {
+    "revision": "d1b37152bacf1885ccc5",
+    "url": "/feedback/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "de20991f1d8d400f441f",
+    "url": "/feedback/static/css/main.e370f72a.chunk.css"
+  },
+  {
+    "revision": "d1b37152bacf1885ccc5",
+    "url": "/feedback/static/js/2.4865cb2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/feedback/static/js/2.4865cb2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de20991f1d8d400f441f",
+    "url": "/feedback/static/js/main.c5e1ccc3.chunk.js"
+  },
+  {
+    "revision": "6bc468c8ae7bd0c29f89",
+    "url": "/feedback/static/js/runtime-main.dad786a5.js"
+  }
+]);
